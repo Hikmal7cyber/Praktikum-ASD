e55299a5177168fe6ec67dc7866b04a735232de0
Muhammad Hikmal Al-Ghifary <mhikmal.ag@gmail.com>
2025-02-21 10:16:22 +0700

    ASD Pertemuan 2 (Percobaan 3)

--- Jobsheet 2/MahasiswaMain15.java
+++ Jobsheet 2/MahasiswaMain15.java
@@ -9,6 +9,18 @@ public class MahasiswaMain15 {
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
+        System.out.println();
         mhs1.tampilkanInformasi();
+
+        System.out.println();
+        Mahasiswa15 mhs2 = new Mahasiswa15("Annisa Nabila","2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+
+        System.out.println();
+        Mahasiswa15 mhsHikmal = new Mahasiswa15("Muhammad Hikmal","244107020074",3.78,"TI 1B");
+        mhsHikmal.tampilkanInformasi();
+        mhsHikmal.updateIpk(5.0);
+        mhsHikmal.tampilkanInformasi();
     }
 }
