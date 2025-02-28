3296fbf514f8eb902982c1bd3235b4a8678430e6
Muhammad Hikmal Al-Ghifary <mhikmal.ag@gmail.com>
2025-02-28 18:11:43 +0700

    ASD Pertemuan 3 (Percobaan 3) done

--- Jobsheet 3/Matakuliah15.java
+++ Jobsheet 3/Matakuliah15.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Matakuliah15 {
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
 
+    public Matakuliah15() {
+    }
+
     public Matakuliah15 (String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+    public void tambahData() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Kode          : ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama          : ");
+        this.nama = sc.nextLine();
+        System.out.print("SKS           : ");
+        this.sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah jam    : ");
+        this.jumlahJam = Integer.parseInt(sc.nextLine());
     }
 }
